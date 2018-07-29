99c73c4b4a99287ad1287d0320445d53f8801e43
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-07-29 19:14:04 -0400

    fix bubblesort method

--- bubblesort.js
+++ bubblesort.js
@@ -1,27 +1,34 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
   let isSorted = false;
   let lastUnsorted = arr.length - 1;
-  while (!isSorted){
+  while (!isSorted) {
     isSorted = true;
-    for (let i = 0; i < lastUnsorted; i++){
-      if (arr[i] > arr[i+1]){
-        swap(arr, i, i + 1);
+    for (let i = 0; i < lastUnsorted; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i);
+        isSorted = false;
       }
     }
     lastUnsorted--;
   }
   return arr;
-  // for (let i = 0; i < arr.length; i++){
-  //   for (let j = 0; j < (arr.length - i - 1); j++){
-  //     if (arr[j] > arr[j+1]){
-  //       swap(arr, j, j+1);
+
+  // different implementation that will run O(n) times by default
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   for (let j = 0; j < arr.length - i - 1; j++) {
+  //     if (arr[j] > arr[j + 1]) {
+  //       swap(arr, j);
   //     }
   //   }
   // }
   // return arr;
-  //helper swap function
+  // helper swap function
 }
 
-function swap(arr, idx1, idx2){
-  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+function swap(array, index) {
+  // side effects
+  const oldLeftValue = array[index];
+  array[index] = array[index + 1];
+  array[index + 1] = oldLeftValue;
 }
